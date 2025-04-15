026445cf20c1335ed1fd7d93609ba7d2913cd3b5
rootTiket <l20078829@gmail.com>
2025-04-15 15:40:10 +0900

    refactor: 파일기반에서 db기반으로 레포지토리 변경

--- src/main/java/org/sopt/repository/PostRepository.java
+++ src/main/java/org/sopt/repository/PostRepository.java
@@ -1,21 +1,10 @@
 package org.sopt.repository;
 
 import java.util.List;
-import java.util.Optional;
+
 import org.sopt.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository {
-    Post save(Post post);
-
-    Optional<Post> findById(int id);
-
-    List<Post> findAll();
-
-    void update(Post post);
-
-    boolean delete(int id);
-
-    boolean isPresent(int id);
-
-    List<Post> findByTitleContaining(String titleKeyword);
+public interface PostRepository extends JpaRepository<Post, Integer> {
+	List<Post> findByTitleContentContaining(String keyword);
 }
