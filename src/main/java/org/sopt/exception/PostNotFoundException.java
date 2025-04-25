2439d8e78ad3f8d0935f3a755d94e69fd403d509
rootTiket <l20078829@gmail.com>
2025-04-25 11:22:19 +0900

    refactor: 사용하지 않는 텍스트 삭제

--- src/main/java/org/sopt/exception/PostNotFoundException.java
+++ src/main/java/org/sopt/exception/PostNotFoundException.java
@@ -4,9 +4,8 @@ import org.sopt.global.exception.BusinessException;
 import org.sopt.global.exception.ExceptionCode;
 
 public class PostNotFoundException extends BusinessException {
-
 	public PostNotFoundException() {
-		super(ExceptionCode.POST_NOT_FOUND, "게시글이 존재하지 않습니다.");
+		super(ExceptionCode.POST_NOT_FOUND);
 	}
 }
 
