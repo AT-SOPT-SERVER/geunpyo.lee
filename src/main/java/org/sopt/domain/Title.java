0542025d2767f25412dc79a282d5eb6601a3af2f
rootTiket <l20078829@gmail.com>
2025-04-28 14:49:35 +0900

    refactor: VO를 record로 변경

--- src/main/java/org/sopt/domain/Title.java
+++ src/main/java/org/sopt/domain/Title.java
@@ -2,44 +2,16 @@ package org.sopt.domain;
 
 import static org.sopt.util.TitleFormatValidateUtil.*;
 
-import java.util.Objects;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
 @Embeddable
-public class Title {
-
+public record Title(
 	@Column(nullable = false)
-	private String content;
-
-	public Title(String content) {
+	String content
+) {
+	public Title {
 		validate(content);
-		this.content = content;
-	}
-
-	public Title() {
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-		Title title = (Title)o;
-		return Objects.equals(content, title.content);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(content);
 	}
 
 	@Override
