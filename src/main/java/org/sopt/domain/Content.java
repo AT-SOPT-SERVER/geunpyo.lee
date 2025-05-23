b25055da348da8feaa98ccbc0eee07c1b024de27
rootTiket <l20078829@gmail.com>
2025-05-23 23:10:01 +0900

    feat: querydsl 세팅

--- src/main/java/org/sopt/domain/Content.java
+++ src/main/java/org/sopt/domain/Content.java
@@ -2,16 +2,50 @@ package org.sopt.domain;
 
 import static org.sopt.util.ContentFormatValidateUtil.*;
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Embeddable
-public record Content(
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public final class Content {
 	@Column(name = "content", nullable = false)
-	String value
-) {
-	public Content {
+	private String value;
+
+	public Content(
+		String value) {
 		validateContentFormat(value);
+		this.value = value;
 	}
+
+	@Column(name = "content", nullable = false)
+	public String value() {
+		return value;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this)
+			return true;
+		if (obj == null || obj.getClass() != this.getClass())
+			return false;
+		var that = (Content)obj;
+		return Objects.equals(this.value, that.value);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(value);
+	}
+
+	@Override
+	public String toString() {
+		return "Content[" +
+			"value=" + value + ']';
+	}
+
 }
 
--- src/main/java/org/sopt/domain/Title.java
+++ src/main/java/org/sopt/domain/Title.java
@@ -2,20 +2,48 @@ package org.sopt.domain;
 
 import static org.sopt.util.TitleFormatValidateUtil.*;
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Embeddable
-public record Title(
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Title {
 	@Column(name = "title", nullable = false)
-	String content
-) {
-	public Title {
+	private String content;
+
+	public Title(
+		String content) {
 		validateTitleFormat(content);
+		this.content = content;
 	}
 
 	@Override
 	public String toString() {
 		return content;
 	}
+
+	@Column(name = "title", nullable = false)
+	public String content() {
+		return content;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this)
+			return true;
+		if (obj == null || obj.getClass() != this.getClass())
+			return false;
+		var that = (Title)obj;
+		return Objects.equals(this.content, that.content);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(content);
+	}
+
 }
