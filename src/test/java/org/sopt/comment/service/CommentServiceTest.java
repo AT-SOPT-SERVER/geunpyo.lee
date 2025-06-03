f0bea56658186252cbbfa0b6cd35bbaf5930eea1
rootTiket <l20078829@gmail.com>
2025-06-04 00:51:55 +0900

    test: 댓글 수정기능 구현

--- src/test/java/org/sopt/comment/service/CommentServiceTest.java
+++ src/test/java/org/sopt/comment/service/CommentServiceTest.java
@@ -10,10 +10,13 @@ import org.junit.jupiter.api.Test;
 import org.sopt.comment.controller.response.CommentResponse;
 import org.sopt.comment.domain.Comment;
 import org.sopt.comment.repository.CommentRepository;
+import org.sopt.comment.service.exception.CommentNotFoundException;
 import org.sopt.comment.service.request.CommentCreateCommand;
+import org.sopt.comment.service.request.CommentUpdateCommand;
 import org.sopt.post.domain.Content;
 import org.sopt.post.domain.Post;
 import org.sopt.post.domain.Title;
+import org.sopt.post.exception.AccessDeniedException;
 import org.sopt.post.repository.PostRepository;
 import org.sopt.user.domain.User;
 import org.sopt.user.repository.UserRepository;
@@ -117,6 +120,63 @@ class CommentServiceTest {
 		assertThat(comment.getParent().getId()).isEqualTo(savedParent.getId());
 		assertThat(comment.getContent()).isEqualTo(command.content());
 		assertThat(comment.getLikes()).isEqualTo(1);
+
+	}
+
+	@DisplayName("사용자는 댓글을 수정할 수 있다.")
+	@Test
+	void update() {
+		//given
+		User user = User.create("test", "test@gmail.com");
+		User user2 = User.create("test2", "test@gmail.com");
+		User savedUser = userRepository.save(user);
+		User savedUser2 = userRepository.save(user2);
+
+		Post post = Post.create(
+			new Title("testtitle"),
+			new Content("testContent"),
+			List.of(BE, ETC),
+			savedUser
+		);
+
+		Post savedPost = postRepository.save(post);
+		Comment comment = commentRepository.save(Comment.createWithoutParent("testComment", savedUser2, savedPost));
+
+		CommentUpdateCommand command = new CommentUpdateCommand("updateComment");
+		//when
+		commentService.updateComment(savedUser2.getId(), comment.getId(), command);
+
+		//then
+		Comment resultComment = commentRepository.findById(comment.getId()).orElseThrow(CommentNotFoundException::new);
+
+		assertThat(resultComment.getContent()).isEqualTo("updateComment");
+
+	}
+
+	@DisplayName("사용자는 다른 사람의 댓글을 수정할 수 없다.")
+	@Test
+	void updateAnotherUserComment() {
+		//given
+		User user = User.create("test", "test@gmail.com");
+		User user2 = User.create("test2", "test@gmail.com");
+		User savedUser = userRepository.save(user);
+		User savedUser2 = userRepository.save(user2);
+
+		Post post = Post.create(
+			new Title("testtitle"),
+			new Content("testContent"),
+			List.of(BE, ETC),
+			savedUser
+		);
+
+		Post savedPost = postRepository.save(post);
+		Comment comment = commentRepository.save(Comment.createWithoutParent("testComment", savedUser2, savedPost));
+
+		CommentUpdateCommand command = new CommentUpdateCommand("updateComment");
+
+		//when & then
+		assertThatThrownBy(() -> commentService.updateComment(savedUser.getId(), comment.getId(), command))
+			.isInstanceOf(AccessDeniedException.class);
 
 	}
 
