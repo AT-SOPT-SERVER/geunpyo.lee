415c901c88c888b63a1505a36629ffc6198b287d
rootTiket <l20078829@gmail.com>
2025-06-06 13:29:06 +0900

    feat: 좋아요 토글 컨트롤러 생성

--- src/main/java/org/sopt/comment/controller/CommentController.java
+++ src/main/java/org/sopt/comment/controller/CommentController.java
@@ -2,12 +2,15 @@ package org.sopt.comment.controller;
 
 import org.sopt.comment.controller.request.CommentCreateRequest;
 import org.sopt.comment.controller.request.CommentUpdateRequest;
+import org.sopt.comment.controller.response.CommentLikeChangeResponse;
 import org.sopt.comment.controller.response.CommentResponse;
+import org.sopt.comment.service.CommentLikeService;
 import org.sopt.comment.service.CommentService;
 import org.sopt.global.common.annotation.Auth;
 import org.sopt.global.common.dto.ResponseDto;
 import org.sopt.user.domain.User;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,15 +18,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/comments")
+@RequiredArgsConstructor
 public class CommentController {
 
 	private final CommentService commentService;
-
-	public CommentController(CommentService commentService) {
-		this.commentService = commentService;
-	}
+	private final CommentLikeService commentLikeService;
 
 	@PostMapping("/posts/{postId}/comments")
 	public ResponseDto<CommentResponse> createComment(User user,
@@ -54,5 +57,12 @@ public class CommentController {
 		@PathVariable Long commentId) {
 		commentService.deleteComment(user, commentId);
 		return ResponseDto.okWithoutContent();
+	}
+
+	@PatchMapping("/likes/{commentId}")
+	public ResponseDto<CommentLikeChangeResponse> toggleLike(@Auth User user,
+		@PathVariable Long commentId) {
+		CommentLikeChangeResponse response = commentLikeService.toggleLike(user, commentId);
+		return ResponseDto.ok(response);
 	}
 }
